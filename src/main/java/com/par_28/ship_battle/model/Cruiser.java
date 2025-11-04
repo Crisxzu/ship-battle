a907f0f72120857bcae3ff5d1dee03153a8f87af
OhBadBoy <james-junior.pamara@epitech.eu>
2025-11-04 15:31:41 +0100

    docs: add comprehensive Javadoc documentation to Cruiser class

--- src/main/java/com/par_28/ship_battle/model/Cruiser.java
+++ src/main/java/com/par_28/ship_battle/model/Cruiser.java
@@ -1,5 +1,14 @@
 package com.par_28.ship_battle.model;
 
+/**
+ * Represents a cruiser ship (Cruiser) in the game.
+ * The cruiser is a large-sized ship with a length of 4 cells.
+ * @author James
+ * @version 1.0
+ * @since 1.0
+ * @see Ship
+ */
+
 public class Cruiser extends Ship {
 
     /**

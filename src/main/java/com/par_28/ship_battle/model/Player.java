d71358bbee5d05e34d535968ed55f83b99eb8949
Crisxzu <chris@snowsky-dev.com>
2025-11-04 14:57:13 +0100

    docs: add detail on player class description

--- src/main/java/com/par_28/ship_battle/model/Player.java
+++ src/main/java/com/par_28/ship_battle/model/Player.java
@@ -7,6 +7,8 @@ import com.par_28.ship_battle.model.exceptions.ShipPlacementException;
 /**
  * Player model representing a game player
  * 
+ * a player has a name, a grid to place ships on, a tracking grid to track attacks on opponent,
+ * 
  * @see com.par_28.ship_battle.model.Grid
  * @see com.par_28.ship_battle.model.Ship
  * @see com.par_28.ship_battle.model.Coordinate

f34f9cf1a92d33ca1ecbd95f01d82882b7ca0a45
Crisxzu <chris@snowsky-dev.com>
2025-11-04 12:10:02 +0100

    feat: add consoleview and consolegamecontroller with support of main menu

--- src/main/java/com/par_28/ship_battle/Main.java
+++ src/main/java/com/par_28/ship_battle/Main.java
@@ -1,5 +1,7 @@
 package com.par_28.ship_battle;
 
+import com.par_28.ship_battle.controller.ConsoleGameController;
+
 /**
  * Main entry point of our game ShipBattle
  */
@@ -12,14 +14,8 @@ public class Main {
      * @param args inline arguments (not used though)
      */
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ConsoleGameController consoleGameController = new ConsoleGameController();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        consoleGameController.launch();
     }
 }

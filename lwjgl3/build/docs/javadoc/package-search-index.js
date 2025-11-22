87c26d22553aa484a30574927a9f787b80f52ab6
Crisxzu <chris@snowsky-dev.com>
2025-11-22 12:17:07 +0100

    refactor: delete build directory and gradle hidden files

--- lwjgl3/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.par_28.ship_battle.lwjgl3"}];updateSearchResults();
--- lwjgl3/build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.par_28.ship_battle.lwjgl3","l":"Lwjgl3Launcher"},{"p":"com.par_28.ship_battle.lwjgl3","l":"StartupHelper"}];updateSearchResults();
